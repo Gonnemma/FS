5156e66a53e42d64125e1fd03e22a8d133ec5e64
Gonzalo Arramon <gonzaarramon7@gmail.com>
2024-08-02 20:17:49 -0300

    ultimo

--- Tareas curso/Trabajo final/src/js/productos.js
+++ Tareas curso/Trabajo final/src/js/productos.js
@@ -3,6 +3,10 @@ let arrPrecios = [3699, 3680, 3199, 1800, 1449, 7200, 1350, 3280, 2989, 943];
 let arrCantidad = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
 let total = 0; // Variable para almacenar el total acumulado
 
+// modal
+var modal = document.getElementById("myModal"); 
+var span = document.getElementsByClassName("close")[0]; 
+
 document.querySelectorAll(".botoncomprar").forEach(button => {
     button.addEventListener("click", event => {
         let productoIndex = parseInt(event.target.getAttribute("data-producto")); // Asegurarse de que es un número
@@ -19,7 +23,26 @@ document.querySelectorAll(".botoncomprar").forEach(button => {
 });
 
 function comprar(cantidad, productoIndex) {
+
+    modal.style.display = "block";
+
     total += cantidad * arrPrecios[productoIndex]; // Actualiza el total acumulado
     document.getElementById("total").innerText = total;
     arrCantidad[productoIndex] -= cantidad; // Actualiza la cantidad disponible
+}
+
+// Modal
+
+// Obtener el elemento <span> que cierra el modal
+// Cuando el usuario haga clic en <span> (x), cierra el modal
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario haga clic en cualquier parte fuera del modal, cierra el modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
